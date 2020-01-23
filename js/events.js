ab337a7cd06fced9b0601fdadc30122e367e740b
mateusfg7 <mateusfelipefg77@gmail.com>
2020-01-23 13:48:20 -0300

    clean code

--- js/events.js
+++ js/events.js
@@ -1,10 +1,10 @@
-var body = $('#body');
-var div = $('#app');
-var styleButton = $('#style');
-var addButton = $('#addButton')
-var input = $('input');
+const body = $('#body');
+const div = $('#app');
+const styleButton = $('#style');
+const addButton = $('#addButton')
+const input = $('input');
 
-var style = JSON.parse(localStorage.getItem('style_mode')) || 0;
+let style = JSON.parse(localStorage.getItem('style_mode')) || 0;
 
 function saveStyleToStorage() {
     localStorage.setItem('style_mode', JSON.stringify(style));
@@ -15,12 +15,14 @@ function darkMode() {
     div.addClass('darkDiv');
     input.addClass('darkElements');
     addButton.addClass('darkElements');
+    
     styleButton.text('Light Mode');
+    
     style = 1;
     saveStyleToStorage()
 }
 
-function lightMode() {
+function lightMode() { 
     body.removeClass('darkBody');
     div.removeClass('darkDiv');
     input.removeClass('darkElements');
@@ -30,23 +32,26 @@ function lightMode() {
     div.addClass('lightDiv');
     input.addClass('lightElements');
     addButton.addClass('lightElements');
+   
     styleButton.text('Dark Mode');
+   
     style = 0;
     saveStyleToStorage()
 }
 
 
-
 if (style == 0) {
     lightMode();
-} else if (style == 1) {
+} 
+else if (style == 1) {
     darkMode();
 }
 
-styleButton.click(function(){
+styleButton.click(() => {
     if (style == 0) {
         darkMode();
-    } else {
+    }
+    else {
         lightMode();
     }
 })
--- js/todo.js
+++ js/todo.js
@@ -1,8 +1,7 @@
-var listElement = document.querySelector('#app ul');
-var inputElement = document.querySelector('#app input');
-var buttonElement = document.querySelector('#app button');
+const listElement = document.getElementById('mainList');
+const inputElement = document.getElementById('inputTodo');
 
-var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+const todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function saveToStorage() {
     localStorage.setItem('list_todos', JSON.stringify(todos));
@@ -11,18 +10,18 @@ function saveToStorage() {
 function renderTodos() {
     listElement.innerHTML = '';
 
-    for(todo of todos) {
-        var todoElement = document.createElement('li');
-        var todoText = document.createTextNode(todo);
+    todos.map(todo => {
+        const todoElement = document.createElement('li');
+        const todoText = document.createTextNode(todo);
 
-        var linkElement = document.createElement('a');
+        const linkElement = document.createElement('a');
 
         linkElement.setAttribute('href', '#');
 
-        var pos = todos.indexOf(todo);
+        const pos = todos.indexOf(todo);
         linkElement.setAttribute('onclick', `deleteTodo(${pos})`)
 
-        var linkText = document.createTextNode(' Excluir');
+        const linkText = document.createTextNode(' Excluir');
 
         linkElement.appendChild(linkText);
         
@@ -30,24 +29,23 @@ function renderTodos() {
         todoElement.appendChild(linkElement);
 
         listElement.appendChild(todoElement);
-    }
+    })
 }
 
-renderTodos();
-
 function addTodo() {
-    var todoText = inputElement.value;
+    const todoText = inputElement.value;
 
     todos.push(todoText);
     inputElement.value = '';
+    
     renderTodos();
     saveToStorage();
 }
 
-buttonElement.onclick = addTodo;
 
 function deleteTodo(pos){
     todos.splice(pos, 1);
+    
     renderTodos();
     saveToStorage();
 }
